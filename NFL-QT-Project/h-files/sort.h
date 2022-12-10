99b50af94a4502887209676f113d104b56e41b5c
Abi <91515654+awhittle2@users.noreply.github.com>
2022-12-10 12:21:20 -0800

    Update and rename NFL-QT-Project/sort.h to NFL-QT-Project/h-files/sort.h

--- NFL-QT-Project/h-files/sort.h
+++ NFL-QT-Project/h-files/sort.h
@@ -6,18 +6,8 @@
 #include <QTableWidgetItem>
 #include "utils.h"
 
-
-/*
- * Sorts the items in the vector, from [start, end), using the specified column.
- * Sorts in ascending order if ascending is true, otherwise descending.
- */
 void sortColumn(QVector<std::array<QTableWidgetItem*, 10>>& rows, int start, int end, int column, bool ascending);
 
-
-/*
- * Checks if two QTableWidgetItems are equal (because it does not give us the
- * ability to do so natively).
- */
 bool operator==(const QTableWidgetItem& first, const QTableWidgetItem& second);
 
-#endif // SORT_H
+#endif
