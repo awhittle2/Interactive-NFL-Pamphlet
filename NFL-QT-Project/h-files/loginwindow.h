a334bb6fd9e0bd4c75389c7917975c8ae4b05d4c
Abi <91515654+awhittle2@users.noreply.github.com>
2022-12-10 12:24:14 -0800

    Update and rename NFL-QT-Project/loginwindow.h to NFL-QT-Project/h-files/loginwindow.h

--- NFL-QT-Project/h-files/loginwindow.h
+++ NFL-QT-Project/h-files/loginwindow.h
@@ -11,16 +11,13 @@ class LoginWindow;
 class LoginWindow : public QDialog
 {
     Q_OBJECT
-
 public:
     explicit LoginWindow(QWidget *parent = nullptr);
     ~LoginWindow();
-
 private slots:
     void on_pushButton_clicked();
-
 private:
     Ui::LoginWindow *ui;
 };
 
-#endif // DIALOG_H
+#endif
