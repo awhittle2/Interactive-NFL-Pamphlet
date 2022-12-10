5752fae10c7cd5d1abf7130ae4a715d3fbebcf24
Abi <91515654+awhittle2@users.noreply.github.com>
2022-12-10 12:23:47 -0800

    Update and rename NFL-QT-Project/csv.h to NFL-QT-Project/h-files/csv.h

--- NFL-QT-Project/h-files/csv.h
+++ NFL-QT-Project/h-files/csv.h
@@ -8,47 +8,11 @@
 
 namespace csv
 {
-
-    //#### Function Declarations ####//
-
-    // Reads an entire line from a csv stream, returning the number of items read.
-    // All items will be pushed to the back of the vector `output`. Ensure that
-    // :param csvStream: is a binary stream with \r\n line endings that are not
-    // shortened to \n for proper data parsing.
-    //
-    // Throws csv::UnexpectedCharacterError if it finds a special character in a
-    // field that is not quoted.
-    // Throws csv::UnclosedQuoteError if it reaches the end of the stream without
-    // finding a closing quote.
-    // Throws csv::UnexpectedEndOfStreamError if the stream ended while the
-    // function was expecting more input.
     std::size_t readLine(std::istream& csvStream, std::vector<std::string>& output, const char sep = ',');
-
-
-    // Takes in a file stream as well as a vector of string vectors and uses it as
-    // an output. If :param strict: is true then it will check to make sure every
-    // line is exactly the same number of characters before adding any of them to
-    // the vector. Vectors will be added for each line using `push_back`.
-    //
-    // If :param lineCount: is not 0, then the function will only read up to the
-    // specified number of lines. Defaults to 0.
-    //
-    // Returns the highest number of entries read.
-    //
-    // Throws std::length_error if in strict mode and the line lengths are not equal.
-    // Internally calls csv::readLine and so will throw the same exceptions.
     std::size_t readStream(std::istream& csvStream, std::vector<std::vector<std::string>>& output, std::size_t lineCount = 0, bool strict = false);
-
-
-    // Identical to csv:readStream except it takes in a file name instead of a stream.
-    //
-    // Throws csv::FileError if the file could not be opened.
+    
     std::size_t readFile(std::string fileName, std::vector<std::vector<std::string>>& output, std::size_t lineCount = 0, bool strict = false);
     std::size_t readFile(const char* fileName, std::vector<std::vector<std::string>>& output, std::size_t lineCount = 0, bool strict = false);
-
-
-
-    //#### Exceptions ####//
 
     class CSVException : public std::logic_error
     {
