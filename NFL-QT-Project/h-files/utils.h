c8c9ed7df45820542987517a5ac77351652cfb25
Abi <91515654+awhittle2@users.noreply.github.com>
2022-12-10 12:21:47 -0800

    Update and rename NFL-QT-Project/utils.h to NFL-QT-Project/h-files/utils.h

--- NFL-QT-Project/h-files/utils.h
+++ NFL-QT-Project/h-files/utils.h
@@ -7,24 +7,10 @@
 #include <array>
 #include <QTableWidgetItem>
 
-
-/*
- * Checks to see if the string is a number that includes commas.
- */
 bool isCommaNumber(QString data);
 
-
-/*
- * Converts a QVariant to an unsigned long long. :param okay: is
- * used to determine if the conversion was successful.
- */
 unsigned long long qvarToULongLong(QVariant var, bool* okay = nullptr);
 
-
-/*
- * Function to load the rows from the file at :param path: into
- * the vector :param out:.
- */
 bool loadRowsFromFile(std::string path, QVector<std::array<QTableWidgetItem*, 10>>& out);
 
-#endif // DESTRUCTION_UTILS_H
+#endif
